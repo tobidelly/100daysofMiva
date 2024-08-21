5edc278af67b0e629350232fbf3099f396268348
tobidelly <tobidelly@gmail.com>
2024-08-21 21:11:59 +0100

    Python lists

---
+++ Day-#2/Python-2/python-list.py
@@ -0,0 +1,24 @@
+# lists are used to store multiple items in a single variable.
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+# You can access the list items by referring to the index number:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+# A list can contain different data types:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+# A list with strings, integers and boolean values:
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+# The list() Constructor
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+# Python Collections (Arrays)
