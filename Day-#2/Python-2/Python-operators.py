3437a9fa030e2ee431a6f7a53592db446dc837dc
tobidelly <tobidelly@gmail.com>
2024-08-21 21:04:18 +0100

    Python Operators

---
+++ Day-#2/Python-2/Python-operators.py
@@ -0,0 +1,47 @@
+# Python Operators
+
+print(10 + 5)
+
+# Python divides the operators into the following groups:
+x = 5
+y = 3
+
+print(x + y)
+
+# Arithmetic operators
+x = 5
+y = 3
+
+print(x - y)
+
+# Python divides the operators into the following groups:
+x = 5
+y = 3
+
+print(x * y)
+
+# Python divides the operators into the following groups:
+x = 12
+y = 3
+
+print(x / y)
+
+# Python divides the operators into the following groups:
+x = 5
+y = 2
+
+print(x % y)
+
+# Python divides the operators into the following groups:
+x = 2
+y = 5
+
+print(x ** y) #same as 2*2*2*2*2
+
+# Python divides the operators into the following groups:
+x = 15
+y = 2
+
+print(x // y)
+
+#the floor division // rounds the result down to the nearest whole number
