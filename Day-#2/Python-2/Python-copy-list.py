bf4505df09ad152f60ccb87fd84156d1e7ade940
tobidelly <tobidelly@gmail.com>
2024-08-22 10:40:25 +0100

    Python Copy Lists

---
+++ Day-#2/Python-2/Python-copy-list.py
@@ -0,0 +1,14 @@
+# Make a copy of a list with the copy() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# Make a copy of a list with the list() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+# Make a copy of a list with the : operator:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
