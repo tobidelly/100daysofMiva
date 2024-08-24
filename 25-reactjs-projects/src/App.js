7245adb58e72a226b0b501e24f3186f507661c3b
tobidelly <tobidelly@gmail.com>
2024-08-24 05:26:44 +0100

    Updating src/app.js for all projects

--- 25-reactjs-projects/src/App.js
+++ 25-reactjs-projects/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+/* import React from 'react'; */
 import './App.css';
+import Accordian from './components/Accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>25 React Projects for #100daysofMiva</h1>
       </header>
+      <Accordian />
+      <RandomColor />
     </div>
   );
 }
