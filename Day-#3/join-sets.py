2a5fa27042889085590e9fc57575fa79f7cb7bcc
tobidelly <tobidelly@gmail.com>
2024-08-26 09:22:12 +0100

    Python Join Sets

--- Day-#3/join-sets.py
+++ Day-#3/join-sets.py
@@ -43,3 +43,79 @@ set2 = {1, 2, 3}
 
 set1.update(set2)
 print(set1)
+
+#Join set1 and set2, but keep only the duplicates
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.intersection(set2)
+print(set3)
+
+#Use & to join two sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 & set2
+print(set3)
+
+#Keep the items that exist in both set1, and set2
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2)
+
+print(set1)
+
+#Join sets that contains the values True, False, 1, and 0, and see what is considered as duplicates
+set1 = {"apple", 1,  "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
+
+set3 = set1.intersection(set2)
+
+print(set3)
+
+#Keep all items from set1 that are not in set2
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.difference(set2)
+
+print(set3)
+
+#Use - to join two sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 - set2
+print(set3)
+
+#Use the difference_update() method to keep the items that are not present in both sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
+
+#Keep the items that are not present in both sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
+
+#Use ^ to join two sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(set3)
+
+#Use the symmetric_difference_update() method to keep the items that are not present in both sets
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.symmetric_difference_update(set2)
+
+print(set1)
