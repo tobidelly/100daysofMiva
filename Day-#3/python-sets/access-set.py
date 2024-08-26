3d677a14154f8a4a95822413bb56c54c9520f3fd
tobidelly <tobidelly@gmail.com>
2024-08-26 09:40:14 +0100

    Reordered

---
+++ Day-#3/python-sets/access-set.py
@@ -0,0 +1,15 @@
+#Loop through set and print variables
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+#Check if "banana" is present in the set
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+#Check if "banana" is NOT present in the set
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
