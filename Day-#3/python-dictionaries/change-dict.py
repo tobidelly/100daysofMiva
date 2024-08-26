8e30759d024e4e19c8fee8d8a9058b134051c3b7
tobidelly <tobidelly@gmail.com>
2024-08-26 10:14:36 +0100

    Python Dictionaries

---
+++ Day-#3/python-dictionaries/change-dict.py
@@ -0,0 +1,15 @@
+#Change the "year" to 2018
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+#Update the "year" of the car by using the update() method
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
