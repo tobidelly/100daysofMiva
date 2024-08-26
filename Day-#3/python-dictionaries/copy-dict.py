6214db74910925a3b29f670dfe3d802c9d999d6e
tobidelly <tobidelly@gmail.com>
2024-08-26 11:07:58 +0100

    Copy Python Dictionary

---
+++ Day-#3/python-dictionaries/copy-dict.py
@@ -0,0 +1,17 @@
+# Dictionaries copy() Method
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# Dictionaries dict() Constructor
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
