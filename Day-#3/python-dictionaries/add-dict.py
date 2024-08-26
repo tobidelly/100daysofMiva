b1bea2738cafe2aec2d07212a32d2c249deadfa1
tobidelly <tobidelly@gmail.com>
2024-08-26 10:46:11 +0100

    Python Add Dictionary Items

--- Day-#3/python-dictionaries/add-dict.py
+++ Day-#3/python-dictionaries/add-dict.py
@@ -1,17 +1,17 @@
-#Change the "year" to 2018
+#
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-thisdict["year"] = 2018
+thisdict["color"] = "red"
 print(thisdict)
 
-#Update the "year" of the car by using the update() method
+#Add a color item to the dictionary by using the update() method
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-thisdict.update({"year": 2020})
-print(thisdict)
+thisdict.update({"color": "red"})
+
