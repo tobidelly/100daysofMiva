70ee2944c4fb452d0134448d366085cd58df730d
tobidelly <tobidelly@gmail.com>
2024-09-03 05:05:42 +0100

    Updated to show all other apps

--- 25-reactjs-projects/src/App.js
+++ 25-reactjs-projects/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-/*import Accordian from './components/Accordian';
+import Accordian from './components/Accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';*/
+import LoadMoreData from './components/load-more-data';
 import TreeView from './components/simple-tree-view';
 import menus from './components/simple-tree-view/data';
 
@@ -14,11 +14,11 @@ function App() {
       <header>
         <h1>25 React Projects for #100daysofMiva</h1>
       </header>
-      {/*<Accordian />*/}
-      {/*<RandomColor />*/}
-      {/*<StarRating />*/}
-      {/*<ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} />*/}
-      {/*<LoadMoreData />*/}
+      <Accordian />
+      <RandomColor />
+      <StarRating />
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} />
+      <LoadMoreData />
       <TreeView menus={menus} />
     </div>
   );
