800716c61c6f29eec64f764f43327033cbcabbef
tobidelly <tobidelly@gmail.com>
2024-09-03 04:58:20 +0100

    Modules Set Up and Updates and updates in Simple Tree View Project

--- 25-reactjs-projects/src/App.js
+++ 25-reactjs-projects/src/App.js
@@ -1,11 +1,12 @@
-/* import React from 'react'; */
+import React from 'react';
 import './App.css';
-import Accordian from './components/Accordian';
+/*import Accordian from './components/Accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
+import LoadMoreData from './components/load-more-data';*/
 import TreeView from './components/simple-tree-view';
+import menus from './components/simple-tree-view/data';
 
 function App() {
   return (
@@ -13,12 +14,12 @@ function App() {
       <header>
         <h1>25 React Projects for #100daysofMiva</h1>
       </header>
-      <Accordian />
-      <RandomColor />
-      <StarRating />
-      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'}/>
-      <LoadMoreData />
-      <TreeView />
+      {/*<Accordian />*/}
+      {/*<RandomColor />*/}
+      {/*<StarRating />*/}
+      {/*<ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} />*/}
+      {/*<LoadMoreData />*/}
+      <TreeView menus={menus} />
     </div>
   );
 }
