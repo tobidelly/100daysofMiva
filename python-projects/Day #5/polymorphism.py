2cc3baadd6eec052b7c76c6830fc75f3e32efdbd
tobidelly <tobidelly@gmail.com>
2024-09-06 05:44:07 +0100

    Polymorphism,py

---
+++ python-projects/Day #5/polymorphism.py
@@ -0,0 +1,10 @@
+# len() returns the number of characters for strings
+x = "Hello World!"
+
+print(len(x))
+
+# len() returns the number of items for tuple
+mytuple = ("apple", "banana", "cherry")
+
+print(len(mytuple))
+
