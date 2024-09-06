58b1380d8efefb30bb3b14942e375c94d50ed1ea
tobidelly <tobidelly@gmail.com>
2024-09-06 06:45:27 +0100

    Python Math

---
+++ python-projects/Day #5/python_math.py
@@ -0,0 +1,16 @@
+# Min and Max functions in Python
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# Absolute value function in Python
+x = abs(-7.25)
+
+print(x)
+
+# Power function in Python
+x = pow(4, 3)
+
+print(x)
