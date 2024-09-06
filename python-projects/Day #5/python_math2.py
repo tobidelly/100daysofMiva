55b818d0b81254f11ef97f7a91cc652f78779fdf
tobidelly <tobidelly@gmail.com>
2024-09-06 06:45:38 +0100

    import math here

---
+++ python-projects/Day #5/python_math2.py
@@ -0,0 +1,18 @@
+# import math module
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+# Ceil and Floor functions in Python
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# Pi constant in Python
+x = math.pi
+
+print(x)
